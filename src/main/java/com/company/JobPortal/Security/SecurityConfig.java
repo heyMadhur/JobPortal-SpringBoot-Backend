5533c317d90850994e15a824e87fb6a35c248f1c
heyMadhur <madhurgupta12112002@gmail.com>
2024-11-28 15:46:24 +0530

    Production SetUp Prod- properties change Deployed Frontend CORS Updated

--- src/main/java/com/company/JobPortal/Security/SecurityConfig.java
+++ src/main/java/com/company/JobPortal/Security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://job-portal-frontend-spring-boot-app.vercel.app"));
         // Allow credentials for secure cross-origin requests
         configuration.setAllowCredentials(true);
         // Allow common HTTP methods
